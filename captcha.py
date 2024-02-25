4c753cfbad92ab544dff7e86c041db8d8ffd42b8
Sneh Bhagat <snehbhagat12@gmail.com>
2024-02-25 11:58:54 +0530

    Added a new text line in the image

--- captcha.py
+++ captcha.py
@@ -1,3 +1,6 @@
-from PIL import Image
+from PIL import Image,ImageDraw
 img = Image.new('RGB',(90,60),color = 'white')
+img.save('blank_image.png')
+draw = ImageDraw.Draw(img)
+draw.text((20,20),"Hello World",fill=(38,38,38))
 img.save('blank_image.png')
